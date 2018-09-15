51e0837031b555b7b951a15177853dd7ec955e06
meng zhao <mzhao15@jhu.edu>
2018-09-15 01:13:45 -0400

    update GetData.py

--- src/Data/GetData.py
+++ src/Data/GetData.py
@@ -5,12 +5,14 @@ urls_path = '2016.txt'
 with open(urls_path, 'r') as urls:
     i = 0
     for url in urls:
-        if i < 4:
+        if i < 3:
             os.system('wget ' + url.strip())
+            url_split = url.split()
+            filename = url_split[-1]
+            os.system('yes | unzip' + filename)
             i += 1
         else:
             break
-os.system('yes "yes"| unzip "*.zip"')
 # for logfile in glob("*.csv"):
 #     print(logfile)
 #     os.sytem('aws s3 mv logfile s3://my-insight-data/logfiles2016/')
