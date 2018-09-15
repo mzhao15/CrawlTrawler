6c894f63ccc22b1131544d7c5e16018d7a31993b
meng zhao <mzhao15@jhu.edu>
2018-09-15 15:21:41 -0400

    Final version of GetData.py

--- src/Data/GetData.py
+++ src/Data/GetData.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 
 urls_path = '2016.txt'
 with open(urls_path, 'r') as urls:
@@ -7,8 +8,9 @@ with open(urls_path, 'r') as urls:
         url_split = url.strip().split('/')
         filename = url_split[-1]
         os.system('yes | unzip ' + filename)
-        csv_file = filename.replace('zip', 'csv')
-        os.system('aws s3 mv ' + csv_file + ' s3://my-insight-data/logfiles2016/')
-        print('sending ' + csv_file + ' to s3')
-        os.system('rm log*')
+        os.system('rm *.zip')
         os.system('rm ' + 'README.txt')
+
+for csv_file in glob('*.csv'):
+    print('sending ' + csv_file + ' to s3')
+    os.system('aws s3 mv ' + csv_file + ' s3://my-insight-data/logfiles2016/')
