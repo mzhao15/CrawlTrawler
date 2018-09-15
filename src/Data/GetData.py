a0fe12582048f73cb84b7df72d6f7b094915e0ff
meng zhao <mzhao15@jhu.edu>
2018-09-15 00:37:01 -0400

    update data download script

--- src/Data/GetData.py
+++ src/Data/GetData.py
@@ -1,21 +1,11 @@
 import os
-import glob
+from glob import glob
 
-urls_path = '/home/meng/Documents/Insight-Meng/DataPreprocessing/URL_list.html'
-
-with open(urls_path, 'r') as urls_file:
-    urls = urls_file.read()
-    urls = urls.split()
-    for i, url in enumerate(urls):
-        print(i)
-        if i > 6 and i < 8:
-            print(url)
-            # os.system('wget ' + url)
-            # os.system('yes yes | unzip "*.zip"')
-        elif i >= 8:
-            break
-os.chdir('/home/meng/Documents/Insight-Meng/DataPreprocessing/')
-print(os.getcwd())
-for file in glob.glob("*.py"):
-    print(file)
-    # os.sytem('aws s3 cp file s3://*****/data/')
+urls_path = '/home/meng/Documents/Insight-Meng/src/Data/2016.txt'
+with open(urls_path, 'r') as urls:
+    for url in urls:
+        os.system('wget ' + url.strip())
+        os.system('yes yes | unzip "*.zip"')
+for logfile in glob("*.csv"):
+    print(logfile)
+    os.sytem('aws s3 mv logfile s3://my-insight-data/logfiles2016/')
