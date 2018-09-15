593ec3f4c9f351462fc9fcc75f1f01f4bc5bfa71
meng zhao <mzhao15@jhu.edu>
2018-09-15 00:55:58 -0400

    update GetData.py

--- src/Data/GetData.py
+++ src/Data/GetData.py
@@ -1,11 +1,16 @@
 import os
 from glob import glob
 
-urls_path = '/home/meng/Documents/Insight-Meng/src/Data/2016.txt'
+urls_path = '2016.txt'
 with open(urls_path, 'r') as urls:
+    i = 0
     for url in urls:
-        os.system('wget ' + url.strip())
-        os.system('yes yes | unzip "*.zip"')
-for logfile in glob("*.csv"):
-    print(logfile)
-    os.sytem('aws s3 mv logfile s3://my-insight-data/logfiles2016/')
+        if i < 4:
+            os.system('wget ' + url.strip())
+            i += 1
+        else:
+            break
+os.system('yes yes | unzip "*.zip"')
+# for logfile in glob("*.csv"):
+#     print(logfile)
+#     os.sytem('aws s3 mv logfile s3://my-insight-data/logfiles2016/')
