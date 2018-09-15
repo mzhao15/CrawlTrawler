8a34295c025f2c476c1de320d186520f4f1d3e46
meng zhao <mzhao15@jhu.edu>
2018-09-15 09:34:26 -0400

    fix bugs

--- src/Data/GetData.py
+++ src/Data/GetData.py
@@ -1,18 +1,14 @@
 import os
-from glob import glob
 
 urls_path = '2016.txt'
 with open(urls_path, 'r') as urls:
-    i = 0
     for url in urls:
-        if i < 3:
-            os.system('wget ' + url.strip())
-            url_split = url.split('/')
-            filename = url_split[-1]
-            os.system('yes | unzip ' + filename)
-            i += 1
-        else:
-            break
-for logfile in glob("*.csv"):
-    os.system('aws s3 mv logfile s3://my-insight-data/logfiles2016/')
-    print('sending csv file to s3')
+        os.system('wget ' + url.strip())
+        url_split = url.strip().split('/')
+        filename = url_split[-1]
+        os.system('yes | unzip ' + filename)
+        csv_file = filename.replace('zip', 'csv')
+        os.system('aws s3 mv ' + csv_file + ' s3://my-insight-data/logfiles2016/')
+        print('sending ' + csv_file + ' to s3')
+        os.system('rm log*')
+        os.system('rm ' + 'README.txt')
