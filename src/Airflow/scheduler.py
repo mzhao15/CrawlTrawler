5e9e4a3ac3b7c1f7e25f666db3d1a7dff3ca70af
meng zhao <mzhao15@jhu.edu>
2018-09-20 00:11:48 -0400

    update scheduler

--- src/Airflow/scheduler.py
+++ src/Airflow/scheduler.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 9, 19),
+    'start_date': datetime(2018, 9, 20),
     'email': ['cliffish0408@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -15,10 +15,12 @@ default_args = {
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    # 'end_date': datetime(2018, 10, 1),
 }
 
-dag = DAG('myaf', default_args=default_args, schedule_interval=timedelta(minutes=15))
+# schedule_interval seems to be the interval between tasks
+
+dag = DAG('myaf', default_args=default_args)  # schedule_interval=timedelta(minutes=15)
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
@@ -32,18 +34,9 @@ t2 = BashOperator(
     retries=3,
     dag=dag)
 
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
-
 t3 = BashOperator(
-    task_id='templated',
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
+    task_id='hello_world',
+    bash_command='echo hello world',
     dag=dag)
 
 t2.set_upstream(t1)
