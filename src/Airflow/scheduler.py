1d2a718f627c4b00728bb534c35629fb0757b235
meng zhao <mzhao15@jhu.edu>
2018-09-19 23:18:06 -0400

    update scheduler

--- src/Airflow/scheduler.py
+++ src/Airflow/scheduler.py
@@ -11,14 +11,14 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1),
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('tutorial', default_args=default_args, schedule_interval=timedelta(minutes=1))
+dag = DAG('tutorial', default_args=default_args, schedule_interval=timedelta(minutes=15))
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(
