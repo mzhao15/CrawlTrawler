371e99f9c0e8384d86bdb176f5c819fa6e2f8778
meng zhao <mzhao15@jhu.edu>
2018-09-19 22:29:08 -0400

    add bash files to run batch processes

---
+++ src/Spark/Total.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /usr/local/spark/bin/
+
+spark-submit --master spark://ip-10-0-0-6:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 --executor-memory 4G sparkbatch_Total.py
