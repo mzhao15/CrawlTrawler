582c4d209362f54cdfb956e2bf8e51812b7fd55f
meng zhao <mzhao15@jhu.edu>
2018-09-25 14:33:50 -0400

    optimized the filter transformation

--- src/Spark/sparkbatch_Total.py
+++ src/Spark/sparkbatch_Total.py
@@ -42,12 +42,12 @@ class CountVisits:
 
     def createtable(self):
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS human (id serial PRIMARY KEY, \
+            "CREATE TABLE IF NOT EXISTS total (id serial PRIMARY KEY, \
                                                 visit_date date, \
                                                 cik varchar(50), \
                                                 num_of_visits int);")
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS robot (id serial PRIMARY KEY, \
+            "CREATE TABLE IF NOT EXISTS human (id serial PRIMARY KEY, \
                                                 visit_date date, \
                                                 cik varchar(50), \
                                                 num_of_visits int);")
@@ -62,6 +62,25 @@ class CountVisits:
             .reduceByKey(lambda v1, v2: v1+v2) \
             .map(lambda count: (count[0].split(','), count[1])) \
             .map(lambda count: (count[0][0], count[0][1], count[1]))
+
+    def totalvisits(self):
+        def inserttotal(records):
+            try:
+                db_conn = psycopg2.connect(**pql_params)
+            except Exception as er1:
+                print('cannot connect to PostgreSQL database\n')
+                print(str(er1))
+            cur = db_conn.cursor()
+            cur.execute(
+                "PREPARE inserts AS INSERT INTO total (visit_date, cik, num_of_visits) \
+                                                        VALUES ($1, $2, $3);")
+            extras.execute_batch(cur, "EXECUTE inserts (%s, %s, %s)", records)
+            cur.execute("DEALLOCATE inserts")
+            db_conn.commit()
+            cur.close()
+            db_conn.close()
+
+        self.counter(self.data).foreachPartition(inserttotal)
 
     def humanvisits(self):
         def inserthuman(records):
@@ -81,71 +100,42 @@ class CountVisits:
             db_conn.close()
 
         filters = self.robot_ip_list
-        self.counter(self.data.filter(
-            lambda line: line[0] not in filters)).foreachPartition(inserthuman)
-
-    def robotvisits(self):
-        def insertrobot(records):
-            try:
-                db_conn = psycopg2.connect(**pql_params)
-            except Exception as er1:
-                print('cannot connect to PostgreSQL database\n')
-                print(str(er1))
-            cur = db_conn.cursor()
-            cur.execute(
-                "PREPARE inserts AS INSERT INTO robot (visit_date, cik, num_of_visits) \
-                                                        VALUES ($1, $2, $3);")
-            extras.execute_batch(cur, "EXECUTE inserts (%s, %s, %s)", records)
-            cur.execute("DEALLOCATE inserts")
-            db_conn.commit()
-            cur.close()
-            db_conn.close()
-
-        filters = self.robot_ip_list
-        self.counter(self.data.filter(
-            lambda line: line[0] in filters)).foreachPartition(insertrobot)
-    # def robotvisits(self):
-    #     filters = self.robot_ip_list
-    #     df = self.data.toDF(['ip', 'date', 'time', 'zone', 'cik', 'accession',
-    #                          'extention', 'code', 'size', 'idx', 'norefer', 'noagent',
-    #                          'find', 'crawler', 'browser'])
-    #     drop_list = ['time', 'zone', 'accession', 'extention', 'code', 'size', 'idx',
-    #                  'norefer', 'noagent', 'find', 'crawler', 'browser']
-    #     df.drop(*drop_list).filter(df.ip.isin(filters)).groupBy('date', 'cik').count() \
-    #         .withColumnRenamed('date', 'visit_date').withColumnRenamed('count', 'num_of_visits') \
-    #         .write \
-    #         .format("jdbc") \
-    #         .option("url", "jdbc:postgresql://mypqlinstance.cewczr0j0xld.us-east-1.rds.amazonaws.com:5432/my_insight_db") \
-    #         .option("dbtable", 'robot') \
-    #         .option("driver", "org.postgresql.Driver") \
-    #         .option("user", "mzhao15") \
-    #         .option("password", "zhaomeng148097") \
-    #         .mode("append") \
-    #         .save()
+        # self.counter(self.data.filter(
+        #     lambda line: line[0] not in filters)).foreachPartition(inserthuman)
+        '''
+        get the number of visits per IP to each company
+        filter out the visits by robot IPs
+        '''
+        newrdd = self.data.map(lambda line: (','.join((line[0], line[1], line[4])), 1)) \
+            .reduceByKey(lambda v1, v2: v1+v2) \
+            .map(lambda count: (count[0].split(','), count[1])) \
+            .map(lambda x: (x[0][0], x[0][1], x[0][2], x[1])) \
+            .filter(lambda x: x[0] not in filters)
+        '''
+        count the number of visits by humans to each company
+        '''
+        newrdd.map(lambda x: (','.join((x[1], x[2])), x[3])) \
+            .reduceByKey(lambda v1, v2: v1+v2) \
+            .map(lambda count: (count[0].split(','), count[1])) \
+            .map(lambda count: (count[0][0], count[0][1], count[1])) \
+            .foreachPartition(inserthuman)
 
     def run(self):
-        ''' get the number of visits by human and robot to each company'''
+        ''' get the total number of visits to each company
+            get the total number of visits by human to each company
+        '''
 
-        # filters = self.robot_ip_list
-        # counts = self.counter(self.data.filter(lambda line: line[0] not in filters)).collect()
-        # extras.execute_batch(self.cur, "INSERT INTO human(visit_date, cik, num_of_visits) \
-        #                      VALUES( %s, %s, %s)", counts)
-        # counts = self.counter(self.data.filter(lambda line: line[0] in filters)).collect()
-        # extras.execute_batch(self.cur, "INSERT INTO robot(visit_date, cik, num_of_visits) \
-        #                      VALUES( %s, %s, %s)", counts)
-        # self.db_conn.commit()
-
+        self.totalvisits()
         self.humanvisits()
-        self.robotvisits()
-
         return
 
 
 if __name__ == '__main__':
-    # get the folder name and filename
+
     if len(sys.argv) > 2:
         print('too many arguments\n')
         exit()
+    # get the folder name and filename
     year, month, day = sys.argv[1].split('-')
     foldername = 'logfiles' + year
     filename = 'log' + ''.join((year, month, day)) + '.csv'
