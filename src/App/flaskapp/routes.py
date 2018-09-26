318b7ded89cca55587443299dd40e40ec16a1282
meng zhao <mzhao15@jhu.edu>
2018-09-25 23:29:45 -0400

    update app folder

--- src/App/flaskapp/routes.py
+++ src/App/flaskapp/routes.py
@@ -1,33 +1,32 @@
-from flask import render_template  # , url_for, flash, redirect
+from flask import render_template, url_for, redirect
 from flaskapp import app
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    imgname = 'jhu.jpg'
     title = 'Home'
-    return render_template('home.html', title=title, imgname=imgname)
+    return render_template('home.html', title=title)
 
 
-@app.route("/search")
-def search():
-    title = 'Search'
-    return render_template('search.html', title=title)
-
-
-@app.route("/map")
-def google_map():
-    title = 'Map'
-    return render_template('map.html', title=title)
-
-
-@app.route("/price")
-def price():
-    title = 'Price'
-    return render_template('price.html', title=title)
-
-
-@app.route("/try")
-def mytest():
-    return "this is a test"
+@app.route("/about")
+def about():
+    title = 'About'
+    return render_template('about.html', title=title)
+#
+#
+# @app.route("/map")
+# def google_map():
+#     title = 'Map'
+#     return render_template('map.html', title=title)
+#
+#
+# @app.route("/price")
+# def price():
+#     title = 'Price'
+#     return render_template('price.html', title=title)
+#
+#
+# @app.route("/try")
+# def mytest():
+#     return "this is a test"
