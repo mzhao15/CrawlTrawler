9a21168531389f9504c4c1536cc74496a0888afc
meng zhao <mzhao15@jhu.edu>
2018-09-28 15:25:59 -0400

    update structure of Class

--- src/Spark/sparkbatch_Total.py
+++ src/Spark/sparkbatch_Total.py
@@ -14,6 +14,25 @@ csv file column names:
 '''
 
 
+def batch_insert(table_name, records):
+    ''' insert processed records into tables in batch '''
+    try:
+        db_conn = psycopg2.connect(**pql_params)
+    except Exception as er1:
+        print('cannot connect to PostgreSQL database\n')
+        print(str(er1))
+    cur = db_conn.cursor()
+    cur.execute(
+        "PREPARE stmt AS INSERT INTO {} (visit_date, cik, num_of_visits) \
+                                                VALUES ($1, $2, $3);".format(table_name))
+    extras.execute_batch(cur, "EXECUTE stmt (%s, %s, %s)", records)
+    cur.execute("DEALLOCATE stmt")
+    db_conn.commit()
+    cur.close()
+    db_conn.close()
+    return
+
+
 class CountVisits:
     def __init__(self, data_path, date):
         sc = SparkContext().getOrCreate()
@@ -31,103 +50,76 @@ class CountVisits:
             print('cannot connect to PostgreSQL database\n')
             print(str(er1))
         self.cur = self.db_conn.cursor(cursor_factory=extras.DictCursor)
-        self.createtable()
+        self.create_table('total')
+        self.create_table('human')
         self.robot_ip_list = self.get_robot_ip()
         self.cur.close()
         self.db_conn.close()
 
     def get_robot_ip(self):
+        ''' get the list of robot ips from database'''
         ip_list = []
         self.cur.execute("SELECT ip FROM robot_ip WHERE detected_date=%s;", (self.date,))
         # self.cur.execute("SELECT ip FROM robot_ip;")
-        records = self.cur.fetchall()  # return a list of lists
+        records = self.cur.fetchall()
         for record in records:
             ip_list.append(record['ip'])
         return ip_list
 
-    def createtable(self):
-        '''create two tables: one for total visits, one for visits by humans only'''
+    def create_table(self, table_name):
+        ''' create a table named as table_name, table schema is predefined '''
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS total (id serial PRIMARY KEY, \
+            "CREATE TABLE IF NOT EXISTS {} (id serial PRIMARY KEY, \
                                                 visit_date date, \
                                                 cik varchar(50), \
-                                                num_of_visits int);")
-        self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS human (id serial PRIMARY KEY, \
-                                                visit_date date, \
-                                                cik varchar(50), \
-                                                num_of_visits int);")
+                                                num_of_visits int);".format(table_name))
         self.db_conn.commit()
         return
 
-    def percompanyperIP(self):
-        ''' get the number of visits to each company by each IP '''
-        ''' input: the parsed raw data: self.data  '''
-        ''' output: (ip+date+cik, count) '''
+    def count_percompany_perIP(self):
+        ''' get the number of visits to each company by each IP
+            input: the parsed raw data: self.data
+            output: (ip+date+cik, count) '''
         return self.data.map(lambda line: (','.join((line[0], line[1], line[4])), 1)) \
             .reduceByKey(lambda v1, v2: v1+v2)
 
-    def totalvisits(self, rdd):
-        def inserttotal(records):
-            ''' insert processed records into "total" in batch '''
-            try:
-                db_conn = psycopg2.connect(**pql_params)
-            except Exception as er1:
-                print('cannot connect to PostgreSQL database\n')
-                print(str(er1))
-            cur = db_conn.cursor()
-            cur.execute(
-                "PREPARE inserts AS INSERT INTO total (visit_date, cik, num_of_visits) \
-                                                        VALUES ($1, $2, $3);")
-            extras.execute_batch(cur, "EXECUTE inserts (%s, %s, %s)", records)
-            cur.execute("DEALLOCATE inserts")
-            db_conn.commit()
-            cur.close()
-            db_conn.close()
+    def total_visit(self, count_percompany_perIP):
+        '''     '''
 
-        # self.counter(self.data).foreachPartition(inserttotal)
-        rdd.map(lambda count: (count[0].split(','), count[1])) \
+        def insert(records):
+            ''' insert processed records into "total" in batch '''
+            batch_insert('total', records)
+
+        count_percompany_perIP.map(lambda count: (count[0].split(','), count[1])) \
             .map(lambda count: (','.join((count[0][1], count[0][2])), count[1])) \
             .reduceByKey(lambda v1, v2: v1+v2) \
             .map(lambda count: (count[0].split(','), count[1])) \
             .map(lambda count: (count[0][0], count[0][1], count[1])) \
-            .foreachPartition(inserttotal)
+            .foreachPartition(insert)
 
-    def humanvisits(self, rdd):
-        ''' input rdd: count of visits per company per IP '''
-        def inserthuman(records):
-            try:
-                db_conn = psycopg2.connect(**pql_params)
-            except Exception as er1:
-                print('cannot connect to PostgreSQL database\n')
-                print(str(er1))
-            cur = db_conn.cursor()
-            cur.execute(
-                "PREPARE inserts AS INSERT INTO human (visit_date, cik, num_of_visits) \
-                                                        VALUES ($1, $2, $3);")
-            extras.execute_batch(cur, "EXECUTE inserts (%s, %s, %s)", records)
-            cur.execute("DEALLOCATE inserts")
-            db_conn.commit()
-            cur.close()
-            db_conn.close()
+    def human_visit(self, count_percompany_perIP):
+        '''  '''
+
+        def insert(records):
+            batch_insert('human', records)
 
         filters = self.robot_ip_list
         # count the number of visits by humans to each company
-        rdd.map(lambda count: (count[0].split(','), count[1])) \
+        count_percompany_perIP.map(lambda count: (count[0].split(','), count[1])) \
             .map(lambda count: (count[0][0], count[0][1], count[0][2], count[1])) \
             .filter(lambda line: line[0] not in filters) \
             .map(lambda line: (','.join((line[1], line[2])), line[3])) \
             .reduceByKey(lambda v1, v2: v1+v2) \
             .map(lambda count: (count[0].split(','), count[1])) \
             .map(lambda count: (count[0][0], count[0][1], count[1])) \
-            .foreachPartition(inserthuman)
+            .foreachPartition(insert)
 
     def run(self):
         ''' get the total number of visits to each company
             get the total number of visits by human to each company'''
-        rdd = self.percompanyperIP()
-        self.totalvisits(rdd)
-        self.humanvisits(rdd)
+        count_percompany_perIP = self.count_percompany_perIP()
+        self.total_visit(count_percompany_perIP)
+        self.human_visit(count_percompany_perIP)
         return
 
 
