06dedbd83bf7a10479cde4f335806e456d914996
meng zhao <mzhao15@jhu.edu>
2018-10-06 19:45:02 -0400

    change end date

--- src/Airflow/daily_scheduler.py
+++ src/Airflow/daily_scheduler.py
@@ -13,7 +13,7 @@ default_args = {
     'owner': 'meng',
     'depends_on_past': False,
     'start_date': datetime(2016, 1, 1),
-    'end_date': datetime(2017, 1, 1),
+    'end_date': datetime(2016, 12, 31),
     'email': ['cliffish0408@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
