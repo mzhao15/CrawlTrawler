aa6be6c5bdc46cc38a6704522bf6f60cfa5f712f
mzhao15 <39840210+mzhao15@users.noreply.github.com>
2018-10-09 14:01:13 -0400

    remove parameters

--- src/Spark/params.py
+++ src/Spark/params.py
@@ -3,9 +3,9 @@ parameters used to connect to aws rds postgresql database
 '''
 
 pql_params = {
-    'database': 'my_insight_db',
-    'user': 'mzhao15',
-    'password': 'zhaomeng148097',
-    'host': 'mypqlinstance.cewczr0j0xld.us-east-1.rds.amazonaws.com',
-    'port': 5432
+    'database': '****',
+    'user': '****',
+    'password': '****',
+    'host': '****',
+    'port': ****
 }
